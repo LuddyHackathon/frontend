002003214be6fb1bcc77bd6d526d6020a07dba96
Darshula <tanmayvijaykulkarni2002@gmail.com>
2024-04-05 23:55:50 +0530

    combined if statement for `Platform`

--- index.js
+++ index.js
@@ -4,35 +4,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { DarkTheme as NavigationDarkTheme, DefaultTheme as NavigationDefaultTheme } from '@react-navigation/native';
 import { MD3DarkTheme, MD3LightTheme, adaptNavigationTheme, PaperProvider } from 'react-native-paper';
 import merge from 'deepmerge';
+
 import { name as appName } from './app.json';
 import App from './src/App';
 
 import iconFont from 'react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf';
 
 import { PreferencesContext } from './src/PreferencesContext';
-
-if (module.hot) {
-  module.hot.accept();
-}
-
-if (Platform.OS === 'web') {// Generate CSS for icons
-  const iconFontStyles = `@font-face {
-    src: url(${iconFont});
-    font-family: MaterialCommunityIcons;
-  }`;
-
-  const style = document.createElement('style');
-  style.type = 'text/css';
-
-  // Append the iconFontStyles to the stylesheet
-  if (style.styleSheet) {
-    style.styleSheet.cssText = iconFontStyles;
-  } else {
-    style.appendChild(document.createTextNode(iconFontStyles));
-  }
-
-  document.head.appendChild(style);
-}
 
 const { LightTheme, DarkTheme } = adaptNavigationTheme({
   reactNavigationLight: NavigationDefaultTheme,
@@ -78,8 +56,28 @@ function Main() {
 
 AppRegistry.registerComponent(appName, () => Main);
 
-if (Platform.OS === 'web') (
+if (Platform.OS === 'web') {
+  if (module.hot) {
+    module.hot.accept();
+  }
+  // Generate CSS for icons
+  const iconFontStyles = `@font-face {
+    src: url(${iconFont});
+    font-family: MaterialCommunityIcons;
+  }`;
+
+  const style = document.createElement('style');
+  style.type = 'text/css';
+
+  // Append the iconFontStyles to the stylesheet
+  if (style.styleSheet) {
+    style.styleSheet.cssText = iconFontStyles;
+  } else {
+    style.appendChild(document.createTextNode(iconFontStyles));
+  }
+
+  document.head.appendChild(style);
   AppRegistry.runApplication(appName, {
     rootTag: document.getElementById('root')
   })
-);
+};

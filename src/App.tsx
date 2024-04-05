f1ad08f33da2025213f4a1fe854fee3636ce6fe6
Darshula <tanmayvijaykulkarni2002@gmail.com>
2024-04-05 21:40:29 +0530

    Separated screens into files. Header still giving type errors.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Card, Text, Paragraph, useTheme, Button } from 'react-native-paper';
-
-import { PreferencesContext } from './PreferencesContext';
+import { NativeStackNavigationOptions, createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useTheme } from 'react-native-paper';
 
 import Header from './screens/Header';
 import HomeScreen from './screens/Home';
+import DetailsScreen from './screens/Details';
 
 export type RootStackParamList = {
   Home: undefined,
@@ -17,53 +16,23 @@ export type RootStackParamList = {
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-
-type ProfileScreenNavigationProp = NativeStackNavigationProp<
-  RootStackParamList,
-  'Home'
->;
-
-type Props = {
-  navigation: ProfileScreenNavigationProp;
-};
-
-const Home = ({ navigation }: Props) => (
-  <Card>
-    <Card.Content>
-      <Text variant='bodyLarge'>Home</Text>
-      <Button mode="contained" onPress={() => navigation.navigate('Details')}>
-        Go to details
-      </Button>
-      <Paragraph>Body content in paragraph</Paragraph>
-    </Card.Content>
-  </Card>
-);
-
-const DetailsScreen = () => (
-  <Card>
-    <Card.Content>
-      <Text variant='bodyLarge'>Details</Text>
-      <Paragraph>Body content in paragraph</Paragraph>
-    </Card.Content>
-  </Card>
-);
-
 export default function App() {
   const theme = useTheme();
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator
-        initialRouteName="Home"
+        initialRouteName='Home'
         screenOptions={{
           header: (props) => {
-            const { navigation, route, options, back } = props;
+            const options: NativeStackNavigationOptions = {};
+            const { navigation, route } = props;
+            const back: string = route.name == 'Home' ? '': route.name;
             const headerProps = { navigation, route, options, back };
             return <Header {...headerProps} />;
           },
         }}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Details' component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { Card, Text, Paragraph, Button } from 'react-native-paper';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
@@ -12,14 +14,18 @@ type Props = {
     navigation: HomeScreenNavigationProp;
 };
 
-export default function HomeScreen({ navigation }: Props) {
-    <Card>
-        <Card.Content>
-            <Text variant='bodyLarge'>Home</Text>
-            <Button mode="contained" onPress={() => navigation.navigate('Details')}>
-                Go to details
-            </Button>
-            <Paragraph>Body content in paragraph</Paragraph>
-        </Card.Content>
-    </Card>
+const HomeScreen: React.FC<Props> = ({ navigation }: Props) => {
+    return (
+        <Card>
+            <Card.Content>
+                <Text variant='bodyLarge'>Home</Text>
+                <Button mode="contained" onPress={() => navigation.navigate('Details')}>
+                    Go to details
+                </Button>
+                <Paragraph>Body content in paragraph</Paragraph>
+            </Card.Content>
+        </Card>
+    );
 }
+
+export default HomeScreen;

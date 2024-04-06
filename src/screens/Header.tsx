d11bb22f7a1862ffb655700b9a26c9572d18ebe0
Darshula <tanmayvijaykulkarni2002@gmail.com>
2024-04-06 12:20:49 +0530

    Conditional back arrow with logo

--- src/screens/Header.tsx
+++ src/screens/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Linking } from 'react-native';
-import { getHeaderTitle } from '@react-navigation/elements';
-import { Appbar, IconButton } from 'react-native-paper';
-import { NativeStackNavigationOptions, NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { Route } from '@react-navigation/native';
+import { getHeaderTitle } from '@react-navigation/elements';
+import { Appbar, IconButton, Button, Icon } from 'react-native-paper';
+import { NativeStackNavigationOptions, NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 import { RootStackParamList } from '../App'
 import { PreferencesContext } from '../PreferencesContext';
@@ -26,17 +26,19 @@ const Header = ({ navigation, route, options, back }: Props) => {
 
     return (
         <Appbar.Header>
-            {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
-            <Appbar.Content title={title} />
+            <Button icon={back ? 'arrow-left' : ''} onPress={navigation.goBack} textColor={isThemeDark ? 'white' : 'black'} disabled={back ? false : true}>
+                <Icon size={25} source='package-variant' /> {/** Change to actual logo */}
+            </Button>
+            <Appbar.Content title={title}/>
             <IconButton
                 icon='github'
-                onPress={() => Linking.openURL('https://github.com/CareerSpeak/CareerSpeak')}
+                onPress={() => Linking.openURL('https://github.com/CareerSpeak')}
             />
             <IconButton
                 icon={isThemeDark ? 'brightness-4' : 'brightness-5'}
                 onPress={toggleTheme}
             />
-        </Appbar.Header>
+        </Appbar.Header >
     );
 };
 

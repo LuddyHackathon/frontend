bc7e9f3bc3ffaf2f5077f71ae82e61cb6e093326
Darshula <tanmayvijaykulkarni2002@gmail.com>
2024-04-08 13:02:24 +0530

    auth0 checkpoint. Stuck on isLoading

--- src/screens/SSO.tsx
+++ src/screens/SSO.tsx
@@ -1,28 +1,43 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+
+import { View } from 'react-native';
+import { Button, Text } from 'react-native-paper';
+import { useAuth0, Auth0Provider } from 'react-native-auth0';
 
 const SSOScreen = () => {
-  const handleSignInWithGoogle = () => {
-    // implement Google sign-in
+  const { authorize, clearSession, user, error, isLoading } = useAuth0();
+  const onLogin = async () => {
+    try {
+      await authorize();
+    } catch (e) {
+      console.log(e);
+    }
   };
 
-  const handleSignInWithGitHub = () => {
-    // implement GitHub sign-in
+  const onLogout = async () => {
+    try {
+      await clearSession();
+    } catch (e) {
+      console.log('Log out cancelled');
+    }
   };
-  const handleSignInWithMicrosoft = () => {
-    // implement MS sign-in
-  };
-  const handleSignInWithLinkedin = () => {
-    // implement Linkedin sign-in
-  };
+
+  if (isLoading) {
+    return <View ><Text>isLoading</Text></View>;
+  }
+
+  const loggedIn = user !== undefined && user !== null;
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Button title="Sign in with Google" onPress={handleSignInWithGoogle} />
-      <Button title="Sign in with GitHub" onPress={handleSignInWithGitHub} />
-      <Button title="Sign in with GitHub" onPress={handleSignInWithMicrosoft} />
-      <Button title="Sign in with GitHub" onPress={handleSignInWithLinkedin} />
-    </View>
+    <Auth0Provider domain={"dev-rl5hkn5dh1vq3jl3.eu.auth0.com"} clientId={"OhJMAoDmC7TeLKe2Y9WMmWJFCkWAeT5C"}>
+      {loggedIn && <Text>You are logged in as {user.name}</Text>}
+      {!loggedIn && <Text>You are not logged in</Text>}
+      {error && <Text>{error.message}</Text>}
+
+      <Button onPress={loggedIn ? onLogout : onLogin}>
+        {loggedIn ? 'Log Out' : 'Log In'}
+      </Button>
+    </Auth0Provider>
   );
 };
 

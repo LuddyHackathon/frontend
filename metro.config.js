a343d2b76d88ba5d0957cb3eeb61281649fc586d
Darshula <tanmayvijaykulkarni2002@gmail.com>
2024-04-09 09:13:12 +0530

    Reverted. This config prevented launch on android

--- metro.config.js
+++ metro.config.js
@@ -3,19 +3,3 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
-
-
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      inlineRequires: false,
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
-  };
-})();

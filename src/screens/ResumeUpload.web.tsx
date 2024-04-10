ffd55db4ccc27dec84f2f53c2a2a7ad9080fd754
Darshula <tanmayvijaykulkarni2002@gmail.com>
2024-04-10 23:44:12 +0530

    Web version for file upload

--- src/screens/ResumeUpload.web.tsx
+++ src/screens/ResumeUpload.web.tsx
@@ -5,41 +5,30 @@ import { Button } from 'react-native-paper';
 import { useFilePicker } from 'use-file-picker';
 
 const ResumeUploadScreen = () => {
-    const onUpload = async (res: any) => {
-        console.log(res)
-        try {
-            let formData = new FormData();
-            formData.append('resumeFile', res);
-            console.log('FORMDATA', {
-                uri: res.content,
-                type: res.type,
-                name: res.name
-            });
-            let result = await fetch('http://192.168.1.168/data', {
-                mode: 'no-cors',
-                method: 'POST',
-                headers: {
-                    'content-type': 'multipart/form-data'
-                },
-                body: formData
-            }).then(() => { console.log('got response') })
-            console.log('RESPONSE', result)
-        } catch (error) {
-            console.error(error);
-        }
-    };
-    const { openFilePicker, plainFiles } = useFilePicker({
-        accept: '.pdf',
-        readAs: 'BinaryString'
-    });
-    return (
-        <View>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
-                <Button mode='outlined' onPress={openFilePicker}>Select Resume</Button>
-                <Button mode='outlined' onPress={() => { onUpload(plainFiles[0]) }}>Upload</Button>
-            </View>
-        </View >
-    )
+  const onUpload = async (res: File) => {
+    try {
+      let formData = new FormData();
+      formData.append('resumeFile', res);
+      let xhr = new XMLHttpRequest();
+      xhr.open('POST', 'http://192.168.1.168/data');
+      xhr.send(formData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const { openFilePicker, plainFiles } = useFilePicker({
+    readAs: 'ArrayBuffer',
+    accept: '.pdf',
+    multiple: false
+  });
+  return (
+    <View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+        <Button mode='outlined' onPress={openFilePicker}>Select Resume</Button>
+        <Button mode='outlined' onPress={() => { onUpload(plainFiles[0]) }}>Upload</Button>
+      </View>
+    </View >
+  )
 }
 
 export default ResumeUploadScreen;

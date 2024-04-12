8e50430c9181a8a64a175bed95913df43f5a82d5
Darshula <tanmayvijaykulkarni2002@gmail.com>
2024-04-12 21:24:17 +0530

    Better return strategy for `XMLHttpRequest`

--- src/DataFetcher.tsx
+++ src/DataFetcher.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-
-export function fetchLanguageResult(fileName: string | null, setLanguageResult: CallableFunction) {
+export function fetchLanguageResult(fileName: string | null, done: CallableFunction) {
   let xhr = new XMLHttpRequest();
   xhr.open('POST', `http://192.168.1.168/grammar?file=${fileName}`);
-  xhr.send(null);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      setLanguageResult(xhr.responseText)
-      console.log(xhr.responseText)
-      return xhr.responseText
-    }
-  }
-}
+  xhr.onload = function () {
+    done(null, JSON.parse(xhr.response));
+  };
+  xhr.onerror = function () {
+    done(JSON.parse(xhr.response));
+  };
+  xhr.send();
+};

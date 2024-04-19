9b113b41c0d2d04c82084943b1425b636e663677
Darshula <tanmayvijaykulkarni2002@gmail.com>
2024-04-19 16:57:02 +0530

    Sending long queries using `FormData`

--- src/DataFetcher.tsx
+++ src/DataFetcher.tsx
@@ -12,13 +12,14 @@ export function fetchLanguageResult(fileName: string | null, done: CallableFunct
 
 export function fetchRecommenderResult(text: string, done: CallableFunction) {
   let xhr = new XMLHttpRequest();
-  xhr.open('POST', `http://192.168.1.168/recommender?text=${text}`);
+  let formData = new FormData();
+  formData.append('text', text);
+  xhr.open('POST', 'http://192.168.1.168/recommender');
   xhr.onload = function () {
-    console.log(xhr.response);
     done(null, JSON.parse(xhr.response));
   };
   xhr.onerror = function () {
     done(JSON.parse(xhr.response));
   };
-  xhr.send();
+  xhr.send(formData);
 }

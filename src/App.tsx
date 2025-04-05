f1d93291062006b3a652d53431c05b90e3952ef8
Darshan2024 <darshanugale10@gmail.com>
2025-04-05 00:29:39 -0400

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import TechnicalInterviewerScreen from './screens/TechnicalInterviewer';
 import HRInterviewerScreen from './screens/HRInterviewer';
 import InterviewerResultsScreen from './screens/InterviewerResults';
 
-
 export type RootStackParamList = {
   CareerSpeak: undefined;
   Authentication: undefined;

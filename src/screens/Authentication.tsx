99242ce520a3ec28919714729f6c8849aa8ce556
Darshula <tanmayvijaykulkarni2002@gmail.com>
2025-04-06 06:39:39 -0400

    Added chatbot screen

--- src/screens/Authentication.tsx
+++ src/screens/Authentication.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Pressable, View, TextInput as NativeTextInput, useWindowDimensions } from 'react-native';
-import { Button, Divider, Surface, Text, TextInput } from 'react-native-paper';
+import { Button, Divider, IconButton, Surface, Text, TextInput } from 'react-native-paper';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 import { RootStackParamList } from '../App';
@@ -28,82 +28,36 @@ const onSubmit = (email: string, password: string, isSignUp: boolean, done: Call
 }
 
 const AuthenticationScreen: React.FC<Props> = ({ navigation }: Props) => {
-  const { height, width } = useWindowDimensions();
-  const { isThemeDark } = React.useContext(PreferencesContext);
+  const response = React.useState<string>("")
 
-  const [isSignUp, setShowSignUp] = React.useState<boolean>(false);
-  const [email, setEmail] = React.useState<string>('');
-  const [password, setPassword] = React.useState<string>('');
+  const sendPrompt = () => {
 
-  const signAction = ['in', 'up'];
-  const changeAction = ['Need an account?', 'Already have an account?'];
-
-  const [hidePass, setHidePass] = React.useState(true);
-
-  const [accessToken, setAccessToken] = useAccessToken();
-
-  const passwordRef = React.useRef<NativeTextInput>(null);
+    return response;
+  }
 
   return (
     <Surface style={{ height: '100%', padding: 25, justifyContent: 'center' }}>
-      <View style={{ height: 600, justifyContent: 'space-evenly', flexDirection: 'row' }}>
-        <Surface elevation={2} style={{ height: '100%', width: width / height > 0.7 ? 360 : '100%', borderRadius: 25, padding: 25 }}>
-          <View style={{ alignItems: 'center', height: 50 }}>
-            {isThemeDark ? LogoDark : LogoLight}
-          </View>
-          <Text variant='headlineSmall' style={{ textAlign: 'center', marginVertical: 25 }}>Sign {signAction[+isSignUp]}</Text>
-          <Divider />
+        <Surface elevation={2} style={{ height: '100%', width: '100%', borderRadius: 25, padding: 25 }}>
           <TextInput
-            label='Email'
-            value={email}
-            autoCorrect={true}
-            autoComplete='email'
-            textContentType='emailAddress'
-            keyboardType='email-address'
-            autoCapitalize='none'
-            enterKeyHint='next'
-            mode='flat'
-            enablesReturnKeyAutomatically={true}
-            blurOnSubmit={false}
-            onChangeText={email => setEmail(email)}
-            onSubmitEditing={() => { passwordRef.current?.focus(); }}
-            style={{ marginVertical: 25 }} />
-          <TextInput
-            label='Password'
-            value={password}
-            ref={passwordRef}
-            autoCorrect={false}
-            textContentType='password'
-            autoComplete='current-password'
-            autoCapitalize='none'
-            enterKeyHint='done'
-            mode='flat'
-            enablesReturnKeyAutomatically={true}
-            blurOnSubmit={false}
-            onChangeText={password => setPassword(password)}
-            secureTextEntry={hidePass}
-            onSubmitEditing={() => { onSubmit(email, password, isSignUp, (token: string) => { setAccessToken(token); }); navigation.navigate('InterviewerHome'); }}
-            right={
-              <TextInput.Icon
-                icon='eye'
-                onPress={() => setHidePass(!hidePass)}
-              />
-            } />
-          <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginVertical: 50 }}>
-            <Button onPress={() => { onSubmit(email, password, isSignUp, (token: string) => { setAccessToken(token); }); navigation.navigate('InterviewerHome'); }} mode='contained'>Submit</Button>
-          </View>
-          <View style={{ display: 'flex', flexDirection: 'row', flex: 1, flexWrap: 'wrap' }}>
-            <Text>{changeAction[+isSignUp]} </Text>
-            <View>
-              <Pressable>
-                <Text theme={{ colors: { onSurface: isThemeDark ? '#6CB1FF' : '#1A0DAB' } }} onPress={() => setShowSignUp(!isSignUp)} >
-                  Sign {signAction[+!isSignUp]}!
-                </Text>
-              </Pressable>
-            </View>
+            value={response[0]}
+            multiline
+            scrollEnabled
+            disabled
+            editable={false}
+            style={{ height: 'auto', flexGrow: 100}}
+          />
+          <View style={{flexDirection: 'row', width: '100%'}}>
+            <TextInput
+              placeholder='Ask a question'
+              multiline={false}
+              style={{flexGrow: 100}}
+            />
+            <IconButton
+              icon="send"
+              onPress={() => { }}
+            />
           </View>
         </Surface>
-      </View>
     </Surface>
   );
 };
